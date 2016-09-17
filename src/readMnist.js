b8f49481c69164c59ed40fcffd26e2320f2af3d5
内藤 謙一 <fine14r3@gmail.com>
2016-09-17 14:52:05 +0900

    file is separated

--- src/readMnist.js
+++ src/readMnist.js
@@ -1,8 +1,9 @@
 
+var file = "data/train-images-idx3-ubyte.gz";
 
 var fs = require('fs');
 var zlib = require('zlib');
-var gzipContent = fs.readFileSync("data/train-images-idx3-ubyte.gz");
+var gzipContent = fs.readFileSync(file);
 zlib.gunzip( gzipContent, function(err,binary) {
 //  fs.writeFileSync("data/train-images-idx3-byte.byte");
   console.log( "length = " + binary.length );
